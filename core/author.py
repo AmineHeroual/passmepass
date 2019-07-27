18a1064a6d1a0ba8d409fdaebcf8cd373aae08c4
thousand <47826962+th0usand@users.noreply.github.com>
2019-07-27 12:05:53 +0900

    Update author.py

--- core/author.py
+++ core/author.py
@@ -1,8 +1,8 @@
 import sys
 import os
 
-__author__ = "1ibr4 twitter https://twitter.com/1ibr4"
+__author__ = "thousand"
 __version__ = "1.0.0"
-__codename__ = "Libra"
+__codename__ = "thousand"
 
-fish = "\033[032mauthor = 1ibr4.   Sunday,June 16th,2019   I made passmepass when I was in the second grade of high school\033[00m"
+fish = "\033[032mauthor = thousand.   Sunday,June 16th,2019   I made passmepass when I was in the second grade of high school\033[00m"
