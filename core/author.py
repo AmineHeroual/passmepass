0fc4c0755aa6af3eb7f6f60337139a1488557ace
rnlioilm <47826962+rnlioilm@users.noreply.github.com>
2020-01-18 01:24:10 +0900

    Update author.py

--- core/author.py
+++ core/author.py
@@ -1,8 +1,8 @@
 import sys
 import os
 
-__author__ = "thousand"
+__author__ = "rnlioilm"
 __version__ = "1.0.0"
-__codename__ = "thousand"
+__codename__ = "rnlioilm"
 
-fish = "\033[032mauthor = thousand.   Sunday,June 16th,2019   I made passmepass when I was in the second grade of high school\033[00m"
+fish = "\033[032mauthor = rnlioilm.   Sunday,June 16th,2019   I made passmepass when I was in the second grade of high school\033[00m"
